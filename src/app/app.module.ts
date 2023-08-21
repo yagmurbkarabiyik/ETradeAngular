72c196ec9ac7ec5d2e86886056cfbdab9192a7d3
Yagmur Karabiyik <yagmurbkarabiyiik@gmail.com>
2023-08-21 16:47:10 +0300

    multiple layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    //BrowserAnimationsModule
     AppRoutingModule,
     AdminModule,
     UiModule
--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasketsComponent } from './baskets/baskets.component';
+import { ProductsModule } from './products/products.module';
+import { BasketsModule } from './baskets/baskets.module';
+import { HomeModule } from './home/home.module';
 
 
 
 @NgModule({
-  declarations: [
-    BasketsComponent
-  ],
+  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductsModule,
+    BasketsModule,
+    HomeModule
   ]
 })
 export class ComponentsModule { }
