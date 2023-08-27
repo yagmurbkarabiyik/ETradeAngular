c01b7e964cb372f578bf6f9147af89cc4f01cac0
Yagmur Karabiyik <yagmurbkarabiyik@gmail.com>
2023-08-27 22:45:50 +0300

    delete direvtive update

--- src/app/directive/admin/delete.directive.ts
+++ src/app/directive/admin/delete.directive.ts
@@ -1,8 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Directive, ElementRef, HostListener, Input, Output, Renderer2, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
@@ -15,9 +17,10 @@ export class DeleteDirective extends BaseComponent {
   constructor(
     private element:ElementRef,
     private _renderer:Renderer2,
-    private productService: ProductService,
+    private httClientService: HttpClientService,
     spinner: NgxSpinnerService,
-    public dialog: MatDialog)
+    public dialog: MatDialog,
+    private alertifyService:AlertifyService)
      {
       super(spinner);
         const img = _renderer.createElement("img");
@@ -30,6 +33,7 @@ export class DeleteDirective extends BaseComponent {
      }
 
      @Input() id: string;
+     @Input() controller:string;
      @Output() callBack: EventEmitter<any> = new EventEmitter();
 
      @HostListener("click ")
@@ -37,16 +41,33 @@ export class DeleteDirective extends BaseComponent {
       this.openDialog(async () => {
         this.showSpinner(SpinnerType.BallAtom);
         const td: HTMLTableCellElement = this.element.nativeElement;
-        await this.productService.delete(this.id);
-        $(td.parentElement).animate({
-          opacity:0,
-          left:"+=50",
-          height:"toogle"
-        }, 700, () => {
-          this.callBack.emit();
+        //await this.productService.delete(this.id);
+        this.httClientService.delete({
+          controller:this.controller
+        }, this.id).subscribe(data => {
+          $(td.parentElement).animate({
+            opacity:0,
+            left:"+=50",
+            height:"toogle"
+          }, 700, () => {
+            this.callBack.emit();
+            this.alertifyService.message("Product deleted!", {
+              dismissOthers:true,
+              messageType: MessageType.Success,
+              position:Position.TopRight
+            })
+            });
+          },(errorResponse: HttpErrorResponse) => {
+            this.hideSpinner(SpinnerType.BallAtom);
+            this.alertifyService.message("Product cannot be deleted!", {
+              dismissOthers:true,
+              messageType: MessageType.Error,
+              position:Position.TopRight
+            })
+          })
+        });
 
-          });
-        })
+      
       
       
      }
